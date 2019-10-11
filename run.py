4f06e9c7df8f85a18a0da8cf343a902668fdc281
LeptoSpira <leptospira489@gmail.com>
2019-10-11 08:57:17 -0400

    Use dotenv to hold token

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from offthedialbot import client
+from dotenv import load_dotenv
+import os
 
 
 def main():
-    client.run('token')
+    load_dotenv()
+    token = os.getenv("TOKEN")
+    client.run(token)
 
 
 if __name__ == '__main__':
