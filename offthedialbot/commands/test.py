9e09604f49a98a84483902e26e3dd3ca9de0b681
LeptoSpira <leptospira489@gmail.com>
2019-10-29 21:21:45 -0400

    Fix dumb thing

--- offthedialbot/commands/test.py
+++ offthedialbot/commands/test.py
@@ -14,5 +14,5 @@ async def main(ctx, content):
     await ui.update()
 
     reply = await ui.get_reply('reaction_add', valids={"\U0001f69b", "\u2702"})
-    ui.embed.add_field(name="reaction", value=reply.emoji)
+    ui.embed.add_field(name="reaction", value=reply[0].emoji)
     await ui.update()
