3168305adb263a01ae668d5ff8e1cc8097dabb92
LeptoSpira <leptospira489@gmail.com>
2019-11-07 13:15:51 -0500

    Add database functions

--- offthedialbot/utils/db.py
+++ offthedialbot/utils/db.py
@@ -27,3 +27,21 @@ class DatabaseHandler:
 
         # Collections
         self.profiles = self.db["profiles"]
+
+    def create_mock_data(self):
+        """Create mock data to test on."""
+        profile = self.empty_profile.copy()
+        profile["ign"] = "LeptoSpira"
+        profile["sw"] = 123412341342
+        profile["cb"] = "S"
+        profile["_id"] = 571494333090496514
+        return self.profiles.insert_one(profile)
+
+    def find_profile(self, *, id):
+        """Find a document in the profiles collection by id."""
+        return self.profiles.find_one({"_id": id})
+
+    def new_profile(self, *, profile, id):
+        """Insert a new profile into the profiles collection with id."""
+        profile["_id"] = id
+        return self.profiles.insert_one(profile)
