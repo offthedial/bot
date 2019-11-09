f9c582f67566f0423a5f9597e761e9c39707e472
LeptoSpira <leptospira489@gmail.com>
2019-11-09 11:13:09 -0500

    Update Docstrings

--- offthedialbot/utils/colors.py
+++ offthedialbot/utils/colors.py
@@ -2,14 +2,14 @@
 
 
 class RoleColor:
-    """Enum holding the colors used in embeds."""
+    """Class holding the colors used in embeds."""
     DIALER = 0xc27c28
     COMPETING = 0xc0a659
     STORYMODE = 0xb198be
 
 
 class AlertStyle:
-    """Enum holding specifically alert style colors."""
+    """Class holding specifically alert style colors."""
     DANGER = 0xf50206
     WARNING = 0xffbe00
     SUCCESS = 0x63ae33
