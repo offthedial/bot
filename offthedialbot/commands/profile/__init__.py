aab217b91085445f9369259af95088cc56b8ca22
LeptoSpira <leptospira489@gmail.com>
2019-11-10 16:18:18 -0500

    Add alert when your profile doesn't exist

--- offthedialbot/commands/profile/__init__.py
+++ offthedialbot/commands/profile/__init__.py
@@ -1,2 +1,15 @@
+import discord
+
+import utils
+
+
 async def main(ctx, arg):
-    pass
+    """$profile command."""
+    profile = utils.dbh.find_profile(id=ctx.author.id)
+    if profile is None:
+        alert = utils.embeds.alert(
+            utils.AlertStyle.DANGER, title="No profile found.", description="You don't have a profile."
+        )
+        await ctx.send(embed=alert)
+        raise utils.exc.CommandCancel
+
