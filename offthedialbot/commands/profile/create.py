200eaa819eecbfefc2a08c4545d36f9f034c1bd9
LeptoSpira <leptospira489@gmail.com>
2019-11-10 15:35:01 -0500

    Partial working profile create command
    playstyles, and database does not work

--- offthedialbot/commands/profile/create.py
+++ offthedialbot/commands/profile/create.py
@@ -1,7 +1,9 @@
+import asyncio
+import re
+
 import discord
 
 import utils
-import re
 
 
 async def main(ctx, arg):
@@ -14,26 +16,56 @@ async def main(ctx, arg):
         raise utils.exc.CommandCancel
 
     profile = utils.dbh.empty_profile.copy()
-    embed = create_profile_embed(ctx, profile)
+    embed = create_status_embed(ctx, profile)
 
     ui = await utils.CommandUI(ctx, embed)
 
-    await get_user_profile(ctx, ui, profile)
+    await get_user_status(ctx, ui, profile)
+    await get_user_playstyles(ctx, ui, profile)
+
+    # utils.dbh.new_profile(profile, ctx.author.id)
     await ui.end(status=True)
 
 
-async def get_user_profile(ctx, ui, profile):
+async def get_user_status(ctx, ui, profile):
     """Get valid message for each rank."""
     for key in profile["status"].keys():
 
         if key != "Ranks":
-            await get_standard_field(ui, profile, key)
+            await get_profile_field(ui, profile, key)
         else:
             await get_rank_field(ui, profile)
 
 
-async def get_standard_field(ui, profile, key):
-    """Prompt the user for a the standard field."""
+async def get_user_playstyles(ctx, ui, profile):
+    """Get the user's playstyle and calculate their, style points."""
+    playstyles = {
+        "frontline": (9, 0, 0),
+        "midline": (0, 9, 0),
+        "backline": (0, 0, 9),
+        "aggressive/slayer": (2, 0, 0),
+        "defensive": (0, 1, 2),
+        "objective": (1, 1, 0),
+        "support": (9, 1, 1),
+        "flex": (1, 1, 1),
+    }
+    ui.embed = discord.Embed(
+        title=f"{ctx.author.display_name}'s Style Points",
+        description=f"Please type all of the playstyles that apply to you, click the \u2705 when done."
+    )
+    ui.embed.add_field(name="Playstyles", value="\n".join([playstyle.capitalize() for playstyle in playstyles]))
+    error_fields = {"title": "Invalid Playstyle.", "description": "Please enter a valid playstyle."}
+
+    tasks = [
+        asyncio.create_task(ui.get_valid_message(lambda r: r.lower() in playstyles.keys(), error_fields)),
+        asyncio.create_task(ui.get_reply('reaction_add', valid_reactions='\u2705'))
+    ]
+    user_playstyles = await wait_user_playstyles(ctx, ui, tasks)
+    profile["style_points"] = await calculate_style_points(user_playstyles, playstyles)
+
+
+async def get_profile_field(ui, profile, key):
+    """Prompt the user for a standard user profile field."""
     instructions = {
         "IGN": 'Please type a valid **IGN**, `(WP*Zada, Lepto)`',
         "SW": 'Please type a valid **SW**, `(0000-0000-0000)`',
@@ -46,13 +78,14 @@ async def get_standard_field(ui, profile, key):
         }
     )
     profile["status"][key] = parse_reply(key, reply.content)
-    ui.embed.add_field(name=key, value=f'`{profile["status"][key]}`')
+    ui.embed.add_field(name=key, value=f'`{profile["status"][key]}`', inline=True)
 
 
 async def get_rank_field(ui, profile):
     """Prompt the user for each of the rank fields."""
     create_instructions = lambda k: f'Please type a valid **__{k}__ Rank**, `(C, A-, S+0, X2350)`'
 
+    ui.embed.add_field(name="Ranks", value="-------", inline=False)
     for key in profile["status"]["Ranks"].keys():
         ui.embed.description = create_instructions(key)
         reply = await ui.get_valid_message(
@@ -63,10 +96,40 @@ async def get_rank_field(ui, profile):
             }
         )
         profile["status"]["Ranks"][key] = parse_reply("Ranks", reply.content)
-        ui.embed.add_field(name=key, value=f'`{profile["status"]["Ranks"][key]}`')
+        ui.embed.add_field(name=key, value=f'`{profile["status"]["Ranks"][key]}`', inline=True)
 
 
-def create_profile_embed(ctx, profile):
+async def wait_user_playstyles(ctx, ui, tasks):
+    """Constantly wait for the user to input playstyles."""
+    user_playstyles = []
+
+    complete = False
+    while not complete:
+        task, reply = await ui.wait_tasks(tasks)
+
+        if task == tasks[0]:
+            content = reply.content.lower()
+
+            if content not in user_playstyles:
+                user_playstyles.append(content)
+            else:
+                user_playstyles.remove(content)
+        else:
+            complete = True
+
+    return user_playstyles
+
+
+async def calculate_style_points(user_playstyles, playstyles):
+    """Calculate a user's style points given their playstyles."""
+    style_points = [0, 0, 0]
+    for playstyle in user_playstyles:
+        style_points += [*playstyles[playstyle]]
+
+    return style_points
+
+
+def create_status_embed(ctx, profile):
     """Create embed for displaying user profile."""
     embed = discord.Embed(title=f"{ctx.author.display_name}'s Status:")
     return embed
