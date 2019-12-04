8aa0fe82e128e294899b7c6612dfeb300167a120
LeptoSpira <leptospira489@gmail.com>
2019-12-04 01:34:35 -0500

    Merge $profile status into $profile

--- offthedialbot/commands/profile/__init__.py
+++ offthedialbot/commands/profile/__init__.py
@@ -1,6 +1,32 @@
-from . import status
+import discord
+
+import utils
 
 
 async def main(ctx):
-    """$profile command."""
-    await status.main(ctx)
+    """Run command for $profile."""
+    profile = utils.dbh.find_profile(id=ctx.author.id)
+    if profile is None:
+        await utils.Alert(
+            ctx, utils.Alert.Style.DANGER, title="No profile found.", description="You don't have a profile."
+        )
+        raise utils.exc.CommandCancel
+
+    embed = create_status_embed(ctx.author.display_name, profile)
+    await ctx.send(embed=embed)
+
+
+def create_status_embed(name, profile):
+    """Create profile embed to display user profile."""
+    embed = discord.Embed(title=f"{name}'s Status", color=utils.colors.Roles.DIALER)
+
+    for key, value in profile["status"].items():
+        if key != "Ranks":
+            value = f"`{value}`"
+        else:  # key == "Ranks":
+            value = "\n".join(
+                [f'**{subkey}:** `{subvalue}`' for subkey, subvalue in profile["status"]["Ranks"].items()]
+            )
+        embed.add_field(name=key, value=value, inline=True if key != "Ranks" else False)
+
+    return embed
