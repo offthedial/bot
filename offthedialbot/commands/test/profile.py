225800aa1ab6af5a6ad156e63cf45cbd33fc4cb7
LeptoSpira <leptospira489@gmail.com>
2019-12-22 00:07:29 -0500

    created test profile command to create mock profile for user instantly

---
+++ offthedialbot/commands/test/profile.py
@@ -0,0 +1,26 @@
+import utils
+
+
+async def main(ctx):
+    """Test command to create a mock profile for the user instantly."""
+    profile = {
+        "status": {
+            "IGN": "Dave",
+            "SW": 111100000000,
+            "Ranks": {
+                "Splat Zones": 1000,
+                "Rainmaker": 2500.0,
+                "Tower Control": 1355.6,
+                "Clam Blitz": 1260.8,
+            },
+        },
+        "style_points": ["flex", "slayer"],  # Groups A, B, and C.
+        "cxp": 22,
+        "meta": {
+            "currently_competing": False,
+            "previous_tourneys": [],
+            "dropout_ban": None,
+        }
+    }
+    utils.dbh.new_profile(profile, ctx.author.id)
+    await utils.Alert(ctx, utils.Alert.Style.SUCCESS, title="Created mock profile", description="Located under name: `Dave`")
