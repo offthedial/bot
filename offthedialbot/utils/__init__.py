939eb6c6c0a1ed9f7b24fc31ce928a69b53dcce1
LeptoSpira <leptospira489@gmail.com>
2019-12-21 23:20:39 -0500

    add emojis utils

--- offthedialbot/utils/__init__.py
+++ offthedialbot/utils/__init__.py
@@ -1,6 +1,7 @@
 """Import common utilites used by all commands of the bot."""
 
 from . import checks
+from . import emojis
 from . import colors
 from . import embeds
 from . import exceptions as exc
---
+++ offthedialbot/utils/emojis.py
@@ -0,0 +1,6 @@
+"""Holds the emojis used."""
+
+
+def digits():
+    """Function returning a list of digit emojis."""
+    return [r"{char}\ufe0f\u20e3" for char in [["\u0031", "\u0032", "\u0033", "\u0034", "\u0035", "\u0036", "\u0037", "\u0038", "\u0039", "\u0030"]]]
