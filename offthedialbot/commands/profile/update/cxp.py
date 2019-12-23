954862e844517f4b1e1031c5985b6618b07235ee
LeptoSpira <leptospira489@gmail.com>
2019-12-23 14:18:27 -0500

    create profile update cxp and stylepoints

---
+++ offthedialbot/commands/profile/update/cxp.py
@@ -0,0 +1,15 @@
+import discord
+
+import utils
+from .. import create, check_for_profile
+
+
+async def main(ctx):
+    """Run command for $profile update."""
+    profile = await check_for_profile(ctx)
+    ui: utils.CommandUI = await utils.CommandUI(ctx, create.create_cxp_embed(ctx))
+
+    profile["cxp"] = await create.get_user_cxp(ui)
+
+    utils.dbh.update_profile(profile, ui.ctx.author.id)
+    await ui.end(True)
