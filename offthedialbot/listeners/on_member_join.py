1f7cf570a85d3de56d30f0d225570466b4f01424
LeptoSpira <leptospira489@gmail.com>
2019-12-24 16:55:40 -0500

    create member join listener

---
+++ offthedialbot/listeners/on_member_join.py
@@ -0,0 +1,14 @@
+import utils
+
+
+async def on_member_join(self, member):
+    if member.guild != self.get_guild(self.OTD):
+        return
+    # Get channel and role
+    channel = self.get_channel(self.Channels.GENERAL)
+    dialer = self.get_role(self.Roles.DIALER)
+    # Add role
+    await member.add_roles(dialer)
+    # Create welcome message
+    welcome = await channel.send(f"Let's welcome {member.mention} to __Off the Dial__! :wave:")
+    await welcome.add_reaction("\U0001f44b")
