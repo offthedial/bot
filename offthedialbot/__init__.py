d942f48f3f38af772a044114d69ae9d9ee7c182b
LeptoSpira <leptospira489@gmail.com>
2019-12-24 16:55:29 -0500

    Add static variables to client class. Closes #8

--- offthedialbot/__init__.py
+++ offthedialbot/__init__.py
@@ -1,4 +1,4 @@
-"""Holds client subclass of discord.ext.Bot, and registeres commands."""
+"""Holds client subclass of discord.ext.Bot, and registers listeners, commands, and cogs."""
 
 from discord.ext.commands import Bot
 
@@ -6,7 +6,31 @@ from offthedialbot import listeners
 from offthedialbot import commands
 from offthedialbot import cogs
 
-client = Bot(command_prefix='$')
+
+class Client(Bot):
+    """commands.Bot subclass containing helpful constants."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.OTD = self.get_guild(374715620052172800)
+
+        class Channels:
+            """Class containing channel objects."""
+            GENERAL = self.get_channel(374715620052172802)
+
+        class Roles:
+            """Class containing role objects."""
+            try:
+                DIALER = self.OTD.get_role(427710343616397322)
+            except AttributeError:
+                DIALER = None
+
+        self.Channels = Channels
+        self.Roles = Roles
+
+
+client = Client(command_prefix='$')
 
 listeners.register_listeners(client)
 commands.register_commands(client)
