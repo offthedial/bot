3b4979629995490570e1a910a85d6de88a26cfdb
LeptoSpira <leptospira489@gmail.com>
2020-02-08 20:01:15 -0500

    Update on_member_join with new role & channel locations

--- offthedialbot/listeners/on_member_join.py
+++ offthedialbot/listeners/on_member_join.py
@@ -1,15 +1,21 @@
 """Contains on_member_join listener."""
+from offthedialbot import utils
 
 
 async def on_member_join(self, member):
     """When a new member joins Off the Dial."""
-    if member.guild != self.get_guild(self.OTD):
+    # Check if it's the correct the server
+    if member.guild != self.OTD:
         return
-    # Get channel and role
-    channel = self.get_channel(self.Channels.GENERAL)
-    dialer = self.get_role(self.Roles.DIALER)
-    # Add role
-    await member.add_roles(dialer)
+    # Get channel and roles
+    channel = utils.channels.general(self)
+    roles = [
+        utils.roles.dialer(self),
+        utils.roles.alerts(self)
+    ]
+    # Add roles
+    for role in roles:
+        await member.add_roles(role)
     # Create welcome message
     welcome = await channel.send(f"Let's welcome {member.mention} to __Off the Dial__! :wave:")
     await welcome.add_reaction("\U0001f44b")
