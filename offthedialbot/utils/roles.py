79efcfacf5f0c8ff9eef6232be2f2e00eba33ccf
LeptoSpira <leptospira489@gmail.com>
2020-02-11 18:28:10 -0500

    Add error handling inside of roles.py

--- offthedialbot/utils/roles.py
+++ offthedialbot/utils/roles.py
@@ -1,16 +1,26 @@
 """Contains commonly used roles."""
+from offthedialbot import log
 
 
 def dialer(client):
     """Dialer role."""
-    client.OTD.get_role(427710343616397322)
+    try:
+        client.OTD.get_role(427710343616397322)
+    except AttributeError:
+        log.logger.warn("Could not get Dialer role object, possibly not in Off the Dial.")
 
 
 def alerts(client):
     """Off the Dial Alerts role."""
-    client.OTD.get_role(479793360530440192)
+    try:
+        client.OTD.get_role(479793360530440192)
+    except AttributeError:
+        log.logger.warn("Could not get Alerts role object, possibly not in Off the Dial.")
 
 
 def competing(client):
     """Competing role."""
-    client.OTD.get_role(415767083691802624)
+    try:
+        client.OTD.get_role(415767083691802624)
+    except AttributeError:
+        log.logger.warn("Could not get Competing role object, possibly not in Off the Dial.")
