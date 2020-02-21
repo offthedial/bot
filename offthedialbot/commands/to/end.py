094f9b2ece2d11aca54422bb79513536af9ca21d
LeptoSpira <leptospira489@gmail.com>
2020-02-20 23:00:55 -0500

    Create tournament end command

--- offthedialbot/commands/to/end.py
+++ offthedialbot/commands/to/end.py
@@ -5,8 +5,14 @@ from offthedialbot import utils
 
 
 async def main(ctx):
-    """Cycle through each of the different alert types."""
+    """End the tournament."""
+    await ctx.trigger_typing()
+    utils.dbh.set_tourney_link(None)  # Remove the tourney link
+    # Update all the profiles
     profiles = utils.dbh.profiles.find({"meta.competing": True}, {"_id": True})
     for p in profiles:
+        # Remove competing role
         await ctx.bot.OTD.get_member(p["_id"]).remove_roles(utils.roles.competing(ctx.bot))
+        # Set competing key to false
         utils.dbh.profiles.update_one({"_id": p["_id"]}, {"$set": {"meta.competing": False}})
+    await utils.Alert(ctx, utils.Alert.Style.SUCCESS, title="Tournament Complete!", description="All roles have been removed, profiles have been updated, and signal strength has been applied.")
