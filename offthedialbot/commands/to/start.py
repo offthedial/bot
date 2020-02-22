a927d035c4121530341b60ab38dd5a060e066426
LeptoSpira <leptospira489@gmail.com>
2020-02-22 14:47:53 -0500

    Update docstring

--- offthedialbot/commands/to/start.py
+++ offthedialbot/commands/to/start.py
@@ -7,7 +7,6 @@ from offthedialbot import utils
 @utils.deco.to_only
 @utils.deco.registration(required=False)
 async def main(ctx):
-    """Commence the tournament!"""
+    """Start the tournament!"""
     ui: utils.CommandUI = await utils.CommandUI(ctx, discord.Embed(title="Commencing tournament..."))
-    # something????
     await ui.end(True)
