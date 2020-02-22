50f3256a2da1ed0790af0e2ce673a784dca4205e
LeptoSpira <leptospira489@gmail.com>
2020-02-22 14:05:44 -0500

    Refactor attendees remove command

--- offthedialbot/commands/to/attendees/remove.py
+++ offthedialbot/commands/to/attendees/remove.py
@@ -8,23 +8,31 @@ from offthedialbot import log
 @utils.deco.to_only
 async def main(ctx):
     """Remove an attendee from the tournament."""
-    ui: utils.CommandUI = await utils.CommandUI(ctx, discord.Embed(title="Player drop.", description="Send a mention of the player."))
-
+    ui: utils.CommandUI = await utils.CommandUI(ctx, discord.Embed(title="Remove attendees.", description="Mention each attendee you want to remove."))
     reply = await ui.get_reply()
+
     for member in reply.mentions:
-        if profile := utils.dbh.find_profile(member.id):
+
+        # Check to make sure the attendee is valid
+        try:
             profile = utils.Profile(profile)
-            if profile.is_competing():
-                link = utils.dbh.get_tourney_link()
-                ui.embed.description = f"Remove `{member.display_name}` from smash.gg at **<{link}>**, then hit the \u2705."
-                await ui.get_reply("reaction_add", valid_reactions=["\u2705"])
-                profile.set_competing(False)
-                utils.dbh.update_profile(profile.dict(), member.id)
-                await member.remove_roles(utils.roles.competing(ctx.bot))
-                await utils.Alert(ctx, utils.Alert.Style.WARNING, title="Drop Complete", description=f"`{member.display_name}` is no longer competing.")
-            else:
-                await utils.Alert(ctx, utils.Alert.Style.WARNING, title="Drop Failed", description=f"`{member.display_name}` is not competing.")
+        except utils.Profile.NotFound:
+            await utils.Alert(ctx, utils.Alert.Style.WARNING, title="Remove attendee failed", description=f"`{member.display_name}` does not own a profile.")
+            continue
         else:
-            await utils.Alert(ctx, utils.Alert.Style.WARNING, title="Drop Failed", description=f"`{member.display_name}` does not own a profile.")
-    
+            if not profile.is_competing():
+                await utils.Alert(ctx, utils.Alert.Style.WARNING, title="Remove attendee failed", description=f"`{member.display_name}` is not competing.")
+                continue
+        
+        # Remove attendee from smash.gg
+        link = utils.dbh.get_tourney_link()
+        ui.embed.description = f"Remove `{member.display_name}` from smash.gg at **<{link}/attendees>**, then hit the \u2705."
+        await ui.get_reply("reaction_add", valid_reactions=["\u2705"])
+        # Set profile to false
+        profile.set_competing(False)
+        utils.dbh.update_profile(profile.dict(), member.id)
+        # Remove roles
+        await member.remove_roles(utils.roles.competing(ctx.bot))
+        
+        await utils.Alert(ctx, utils.Alert.Style.SUCCESS, title="Remove attendee complete", description=f"`{member.display_name}` is no longer competing.")
     await ui.end(None)
