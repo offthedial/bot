965356fdbb6640d1abb91c649461d6dedd6750ac
LeptoSpira <leptospira489@gmail.com>
2020-02-23 15:17:02 -0500

    Update alerts and add test help

---
+++ offthedialbot/commands/test/help.py
@@ -0,0 +1,15 @@
+"""$test"""
+import discord
+
+from offthedialbot import utils
+
+
+async def main(ctx):
+    """Commands to help with testing the bot."""
+    await utils.Alert(ctx, utils.Alert.Style.INFO,
+        title="Having trouble with the bot?",
+        description="\n".join([
+            "All directions are __in the embed__!",
+            "That means, to navigate the commands, simply read the bot message!",
+            "If you are still stuck, you can ask for help in #helpdesk."
+    ]))
