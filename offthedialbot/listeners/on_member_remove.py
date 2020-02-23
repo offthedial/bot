4f2d2a8e01f2a7092ce9b75c6486263b6d16a541
LeptoSpira <leptospira489@gmail.com>
2020-02-22 22:28:28 -0500

    Send warning when competing member leaves the guild

---
+++ offthedialbot/listeners/on_member_remove.py
@@ -0,0 +1,30 @@
+"""Contains on_member_remove listener."""
+from offthedialbot import utils
+
+
+async def on_member_remove(self, member):
+    """When a new member leaves Off the Dial."""
+    # Check if it's the correct the server
+    if member.guild != self.OTD:
+        return
+    
+    await warn_if_competing(member)
+
+
+async def warn_if_competing(member):
+    try:
+        profile = utils.Profile(member.id)
+
+    except utils.Profile.NotFound:
+        return
+    if not (profile.get_competing() and not profile.get_banned()):
+        return
+    if not ((tourney := utils.dbh.get_tourney()) and tourney["reg"]):
+        return
+
+    # Member should be warned
+    alert = utils.Alert.create_embed(
+        utils.Alert.Style.WARNING,
+        title="You are still registered for the upcoming tournament!",
+        description="Make sure to rejoin the server immediately or you will be disqualified. If you were intending to droppout, make sure you let the TO's know so they can remove you from the smash.gg page")
+    await member.send(embed=alert)
