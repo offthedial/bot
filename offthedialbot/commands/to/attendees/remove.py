9f56c7de209412c83c1073ee199088d62e65818d
LeptoSpira <leptospira489@gmail.com>
2020-02-26 21:43:18 -0500

    Fix remove.py

--- offthedialbot/commands/to/attendees/remove.py
+++ offthedialbot/commands/to/attendees/remove.py
@@ -23,26 +23,26 @@ async def main(ctx):
         ui.embed.description = f"Remove `{attendee.display_name}` from smash.gg at **<{link}/attendees>**, then hit the \u2705."
         await ui.get_reply("reaction_add", valid_reactions=["\u2705"])
         # Remove attendee from discord
-        await remove_attendee(ctx, attendee, profile, f"attendee manually removed by {ctx.author.display_name}.")
+        await remove_attendee(ctx, attendee, profile, reason=f"attendee manually removed by {ctx.author.display_name}.")
 
         await utils.Alert(ctx, utils.Alert.Style.SUCCESS, title="Remove attendee complete", description=f"`{attendee.display_name}` is no longer competing.")
     await ui.end(None)
 
 
 async def check_valid_attendee(ctx, attendee):
-        try:
-            profile = utils.Profile(attendee.id)
-        except utils.Profile.NotFound:
-            profile = None
-        check = {
-            (lambda: not profile): f"`{attendee.display_name}` does not own a profile.",
-            (lambda: not profile or not profile.get_competing()): f"`{attendee.display_name}` is not competing."
-        }
-        if any(values := [value for key, value in check.items() if key()]):
-            await utils.Alert(ctx, utils.Alert.Style.WARNING, title="Registration Failed.", description=values[0])
-            return False
+    try:
+        profile = utils.Profile(attendee.id)
+    except utils.Profile.NotFound:
+        profile = None
+    check = {
+        (lambda: not profile): f"`{attendee.display_name}` does not own a profile.",
+        (lambda: not profile or not profile.get_competing()): f"`{attendee.display_name}` is not competing."
+    }
+    if any(values := [value for key, value in check.items() if key()]):
+        await utils.Alert(ctx, utils.Alert.Style.WARNING, title="Removal Failed.", description=values[0])
+        return False
 
-        return profile
+    return profile
 
 
 async def remove_attendee(ctx, attendee, profile, *, reason="attendee isn't competing anymore."):
