fc9ca6f47f3add193093a8f11ee4a4216ea45fc7
LeptoSpira <leptospira489@gmail.com>
2020-02-29 22:25:49 -0500

    Add to profiles command group

---
+++ offthedialbot/commands/to/profiles/__init__.py
@@ -0,0 +1,15 @@
+"""$to profiles"""
+from discord.ext import commands
+
+from offthedialbot import utils
+
+
+@utils.deco.require_role("Organiser")
+async def main(ctx):
+    """Command tools for managing profiles."""
+    raise commands.TooManyArguments
+
+def user_and_profile(ctx):
+    """Create a list of tuples containing an user's member object, and profile."""
+    profiles: list = [profile["_id"] for profile in utils.dbh.profiles.find({}, {"_id": True})]
+    return [(ctx.guild.get_member(user_id), utils.Profile(user_id)) for user_id in profiles]
