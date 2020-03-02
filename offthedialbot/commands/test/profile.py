235c635c8c7539fbe18172ebaacf593477e6e36f
LeptoSpira <leptospira489@gmail.com>
2020-03-01 22:02:37 -0500

    Update $test profile

--- offthedialbot/commands/test/profile.py
+++ offthedialbot/commands/test/profile.py
@@ -26,29 +26,8 @@ async def main(ctx):
             "banned": None,
         }
     }
-    profile2: dict = {
-        "_id": 582721579281154079,
-        "status": {
-            "IGN": "Dave",
-            "SW": '048922225689',
-            "Ranks": {
-                "Splat Zones": 1000,
-                "Rainmaker": 2500.0,
-                "Tower Control": 1355.6,
-                "Clam Blitz": 1260.8,
-            },
-        },
-        "stylepoints": ["flex", "slayer"],  # Groups A, B, and C.
-        "cxp": 22,
-        "signal_strength": 150,
-        "meta": {
-            "competing": False,
-            "smashgg": None,
-            "banned": None,
-        }
-    }
+
     utils.dbh.profiles.replace_one({"_id": ctx.author.id}, profile, upsert=True)
-    utils.dbh.profiles.replace_one({"_id": 582721579281154079}, profile2, upsert=True)
     await utils.Alert(
         ctx, utils.Alert.Style.SUCCESS,
         title="Created mock profile", description="Located under name: `Dave`"
