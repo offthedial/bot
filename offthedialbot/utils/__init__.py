6fb2aa41cae9fb771f6e2d2b245e09b0e3617f77
LeptoSpira <leptospira489@gmail.com>
2020-03-02 23:24:02 -0500

    Move database import to the top

--- offthedialbot/utils/__init__.py
+++ offthedialbot/utils/__init__.py
@@ -1,5 +1,6 @@
 """Import common utilites used by all commands of the bot."""
 
+from .db import dbh
 from . import checks
 from . import colors
 from . import emojis
@@ -7,7 +8,6 @@ from . import channels
 from . import roles
 from . import time
 from . import exceptions as exc
-from .db import dbh
 from .alert import Alert
 from .command_ui import CommandUI
 from .profile import Profile
