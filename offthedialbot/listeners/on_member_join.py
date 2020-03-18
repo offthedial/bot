6ca60f7ca13f61689516d1e1354164b91576b533
LeptoSpira <leptospira489@gmail.com>
2020-03-18 13:11:38 -0400

    Docstrings, Linting & Fix timeout error with on_member_join

--- offthedialbot/listeners/on_member_join.py
+++ offthedialbot/listeners/on_member_join.py
@@ -1,22 +1,23 @@
 """Contains on_member_join listener."""
+import asyncio
+
 from offthedialbot import utils
 
 
 async def on_member_join(client, member):
     """When a new member joins Off the Dial."""
-    # Check if it's the correct the server
-    if member.guild != client.OTD:
+    if member.guild != client.OTD:  # Check if it's the correct the server
         return
-    
-    # Add roles
-    await add_roles(client, member)
-    # Create welcome message
-    welcome = await send_welcome(client, member)
+
+    await add_roles(client, member)  # Add roles
+    welcome = await send_welcome(client, member)  # Create welcome message
+
     # Incase they leave immediately
     await member_leave(client, member, welcome)
 
 
 async def add_roles(client, member):
+    """Add roles to the user."""
     try:
         profile = utils.Profile(member.id)
     except utils.Profile.NotFound:
@@ -33,6 +34,7 @@ async def add_roles(client, member):
 
 
 async def send_welcome(client, member):
+    """Send welcome message."""
     channel = utils.channels.general(client)
     welcome = await channel.send(f"Let's welcome {member.mention} to __Off the Dial__! :wave:")
     await welcome.add_reaction("\U0001f44b")
@@ -40,9 +42,10 @@ async def send_welcome(client, member):
 
 
 async def member_leave(client, member, welcome):
+    """Delete the welcome message because the user left less than 5 minutes after."""
     try:
         await client.wait_for('member_remove', check=utils.checks.member(member), timeout=300)
-    except TimeoutError:
+    except asyncio.TimeoutError:
         pass
     else:
         await welcome.delete()
