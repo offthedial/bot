d2dcdeafc664ebd5f071c1b6bfcdaaf49e0ea879
LeptoSpira <leptospira489@gmail.com>
2020-03-26 19:27:46 -0400

    Fix bug with test mutate

--- offthedialbot/commands/test/mutate.py
+++ offthedialbot/commands/test/mutate.py
@@ -5,20 +5,23 @@ from offthedialbot import utils
 @utils.deco.require_role("Developer")
 async def main(ctx):
     profiles = utils.dbh.profiles.find({})
+
+    ps = []
+    mps = []
+
     for profile in profiles:
 
-        utils.dbh.profiles.replace_one({"_id": profile["_id"]}, {
+        ps.append({
             "_id": profile["_id"],
-            "IGN": profiles["status"]["IGN"],
-            "SW": profiles["status"]["SW"],
-            "Ranks": profiles["status"]["Ranks"],
-            "stylepoints": profiles["stylepoints"],
-            "cxp": profiles["cxp"],
-            "signal": profiles["signal_strength"],
+            "IGN": profile["status"]["IGN"],
+            "SW": profile["status"]["SW"],
+            "Ranks": profile["status"]["Ranks"],
+            "stylepoints": profile["stylepoints"],
+            "cxp": profile["cxp"],
+            "signal": profile["signal_strength"],
         })
-
-        utils.dbh.metaprofiles.insert_one({
-            "_id": profiles["_id"],
+        mps.append({
+            "_id": profile["_id"],
             "banned": profile["meta"]["banned"],
             "smashgg": profile["meta"]["smashgg"],
             "reg": {
@@ -26,4 +29,9 @@ async def main(ctx):
                 "code": profile["meta"]["confirmation_code"],
             }
         })
+    utils.dbh.profiles.remove({})
+    utils.dbh.metaprofiles.remove({})
+    utils.dbh.profiles.insert_many(ps)
+    utils.dbh.metaprofiles.insert_many(mps)
+
     await utils.Alert(ctx, utils.Alert.Style.SUCCESS, title="Mutate complete.", description="Remove this command as soon as possible")
