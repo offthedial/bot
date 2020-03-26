93e48801e5bcd78f7ea04554b568158742c49aca
LeptoSpira <leptospira489@gmail.com>
2020-03-26 18:41:39 -0400

    Fix duplicated code segment

--- offthedialbot/commands/profile/update/__init__.py
+++ offthedialbot/commands/profile/update/__init__.py
@@ -7,8 +7,14 @@ from .. import create, display_field, create_status_embed
 async def main(ctx):
     """Update your profile."""
     profile: utils.Profile = utils.Profile(ctx.author.id)
-    embed, emojis = create_update_embed(ctx, profile)
+    await update_profile(ctx, profile)
 
+
+async def update_profile(ctx, profile, title=None):
+    """Update profile given profile."""
+    embed, emojis = create_update_embed(ctx, profile)
+    if title:
+        embed.title = title
     ui: utils.CommandUI = await utils.CommandUI(ctx, embed)
 
     reply = await ui.get_valid_reaction(emojis)
