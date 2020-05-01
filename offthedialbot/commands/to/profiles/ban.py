1c23a32b52f5da791b2c26bda7a51bc8c835c465
LeptoSpira <leptospira489@gmail.com>
2020-05-01 18:35:17 -0400

    Update $to profiles commands

--- offthedialbot/commands/to/profiles/ban.py
+++ offthedialbot/commands/to/profiles/ban.py
@@ -2,55 +2,59 @@
 import discord
 
 from offthedialbot import utils
-from ..attendees import remove
+from ..attendees.remove import ToAttendeesRemove
 
 
-@utils.deco.require_role("Organiser")
-async def main(ctx):
+class ToProfilesBan(utils.Command):
     """Ban an attendee from the tournament."""
-    ui: utils.CommandUI = await utils.CommandUI(ctx,
-        discord.Embed(
-            title="Ban attendees.",
-            description="Mention each attendee you want to ban.",
-            color=utils.Alert.Style.DANGER))
-    reply = await ui.get_valid_message(lambda m: len(m.mentions) >= 1,
-        {"title": "Invalid Mention", "description": "Make sure to send a **mention** of the attendee."})
 
-    for attendee in reply.mentions:
+    @classmethod
+    @utils.deco.require_role("Organiser")
+    async def main(cls, ctx):
+        """Ban an attendee from the tournament."""
+        ui: utils.CommandUI = await utils.CommandUI(ctx,
+            discord.Embed(
+                title="Ban attendees.",
+                description="Mention each attendee you want to ban.",
+                color=utils.Alert.Style.DANGER))
+        reply = await ui.get_valid_message(lambda m: len(m.mentions) >= 1,
+            {"title": "Invalid Mention", "description": "Make sure to send a **mention** of the attendee."})
 
-        profile = utils.ProfileMeta(attendee.id)
+        for attendee in reply.mentions:
 
-        until = await set_ban_length(ui, profile)
-        await remove_smashgg(ui, attendee)
-        await remove.from_competing(ctx, attendee, profile,
-            reason=f"attendee manually banned by {ctx.author.display_name}.")
+            profile = utils.ProfileMeta(attendee.id)
 
-        # Complete ban
-        await utils.Alert(ctx, utils.Alert.Style.SUCCESS,
-            title="Ban attendee complete",
-            description=f"`{attendee.display_name}` is now banned {f'until `{until} UTC`' if until != True else 'forever'}.")
+            until = await cls.set_ban_length(ui, profile)
+            await cls.remove_smashgg(ui, attendee)
+            await ToAttendeesRemove.from_competing(ctx, attendee, profile,
+                reason=f"attendee manually banned by {ctx.author.display_name}.")
 
-    await ui.end(None)
+            # Complete ban
+            await utils.Alert(ctx, utils.Alert.Style.SUCCESS,
+                title="Ban attendee complete",
+                description=f"`{attendee.display_name}` is now banned {f'until `{until} UTC`' if until != True else 'forever'}.")
 
+        await ui.end(None)
 
-async def remove_smashgg(ui, attendee):
-    """Remove attendee from smash.gg if applicable."""
-    try:
-        await remove.from_smashgg(ui, attendee)
-    except TypeError:
-        pass
+    @classmethod
+    async def remove_smashgg(cls, ui, attendee):
+        """Remove attendee from smash.gg if applicable."""
+        try:
+            await ToAttendeesRemove.from_smashgg(ui, attendee)
+        except TypeError:
+            pass
 
+    @classmethod
+    async def set_ban_length(cls, ui: utils.CommandUI, profile):
+        """Get ban length and set it inside of the profile."""
+        ui.embed.description = f"Specify the length of the ban. or enter 'forever' for a permanent ban."
+        ui.embed.add_field(name="Supported symbols:", value=utils.time.User.symbols)
 
-async def set_ban_length(ui: utils.CommandUI, profile):
-    """Get ban length and set it inside of the profile."""
-    ui.embed.description = f"Specify the length of the ban. or enter 'forever' for a permanent ban."
-    ui.embed.add_field(name="Supported symbols:", value=utils.time.User.symbols)
+        parse = lambda m: utils.time.User.parse(m.content) if m.content != "forever" else True
+        reply = await ui.get_valid_message(parse,
+            {"title": "Invalid Length", "description": "Please check the `Supported symbols` and make sure your input is correct."})
+        until = parse(reply)
+        profile.set_banned(until)
 
-    parse = lambda m: utils.time.User.parse(m.content) if m.content != "forever" else True
-    reply = await ui.get_valid_message(parse,
-        {"title": "Invalid Length", "description": "Please check the `Supported symbols` and make sure your input is correct."})
-    until = parse(reply)
-    profile.set_banned(until)
-
-    ui.embed.remove_field(0)
-    return until
+        ui.embed.remove_field(0)
+        return until
--- offthedialbot/commands/to/profiles/restore.py
+++ offthedialbot/commands/to/profiles/restore.py
@@ -10,92 +10,96 @@ from offthedialbot import utils
 from offthedialbot.commands.profile.create import ProfileCreate
 
 
-async def main(ctx):
+class ToProfilesRestore(utils.Command):
     """Temporary command to import from backup."""
-    ui: utils.CommandUI = await utils.CommandUI(ctx, discord.Embed(
-        title="Please upload a valid profiles export."
-    ))
-    reply: discord.Message = await ui.get_valid('message',
-        lambda m: getattr(m, "attachments", []),
-        {"title": "Invalid profiles export", "description": "Please upload a `profiles.csv`."},
-        delete=False)
 
-    async with ctx.typing():
+    @classmethod
+    async def main(cls, ctx):
+        """Temporary command to import from backup."""
+        ui: utils.CommandUI = await utils.CommandUI(ctx, discord.Embed(
+            title="Please upload a valid profiles export."
+        ))
+        reply: discord.Message = await ui.get_valid('message',
+            lambda m: getattr(m, "attachments", []),
+            {"title": "Invalid profiles export", "description": "Please upload a `profiles.csv`."},
+            delete=False)
 
-        reader = csv.reader(await create_file(reply))
-        await reply.delete()
+        async with ctx.typing():
 
-        profiles, metaprofiles = new_profiles(reader)
-        utils.dbh.profiles.insert_many(profiles)
-        utils.dbh.metaprofiles.insert_many(metaprofiles)
+            reader = csv.reader(await cls.create_file(reply))
+            await reply.delete()
 
-    await ui.end(True)
+            profiles, metaprofiles = cls.new_profiles(reader)
+            utils.dbh.profiles.insert_many(profiles)
+            utils.dbh.metaprofiles.insert_many(metaprofiles)
 
+        await ui.end(True)
 
-async def create_file(reply):
-    """Create csv file from reply."""
-    async with aiohttp.ClientSession() as session:  # Get file from url
-        async with session.get(reply.attachments[0].url) as resp:
-            file = StringIO((await resp.content.read()).decode("utf-8"))
-    return file
+    @classmethod
+    async def create_file(cls, reply):
+        """Create csv file from reply."""
+        async with aiohttp.ClientSession() as session:  # Get file from url
+            async with session.get(reply.attachments[0].url) as resp:
+                file = StringIO((await resp.content.read()).decode("utf-8"))
+        return file
 
+    @classmethod
+    def new_profiles(cls, reader):
+        """Return a list of profiles from csv reader."""
+        profiles = []
+        metaprofiles = []
 
-def new_profiles(reader):
-    """Return a list of profiles from csv reader."""
-    profiles = []
-    metaprofiles = []
+        for i, row in enumerate(reader):
+            if (_id := cls.skip(i, row)) is True:
+                continue
 
-    for i, row in enumerate(reader):
-        if (_id := skip(i, row)) is True:
-            continue
+            profiles.append({
+                "_id": _id,
+                "IGN": row[cls.Column.IGN],
+                "SW": ProfileCreate.parse_reply('SW', row[cls.Column.SW]),
+                "Ranks": {
+                    "Splat Zones": ProfileCreate.parse_reply('Ranks', row[cls.Column.SZ]),
+                    "Tower Control": ProfileCreate.parse_reply('Ranks', row[cls.Column.TC]),
+                    "Rainmaker": ProfileCreate.parse_reply('Ranks', row[cls.Column.RM]),
+                    "Clam Blitz": ProfileCreate.parse_reply('Ranks', row[cls.Column.CB]),
+                },
+                "stylepoints": eval(row[cls.Column.SP]),
+                "cxp": int(row[cls.Column.CXP]),
+            })
+            metaprofiles.append({
+                "_id": _id,
+                "signal": int(row[cls.Column.SS]),
+                "smashgg": None,
+                "banned": None,
+                "reg": {
+                    "reg": False,
+                    "code": None,
+                }
+            })
+        return profiles, metaprofiles
 
-        profiles.append({
-            "_id": _id,
-            "IGN": row[Column.IGN],
-            "SW": ProfileCreate.parse_reply('SW', row[Column.SW]),
-            "Ranks": {
-                "Splat Zones": ProfileCreate.parse_reply('Ranks', row[Column.SZ]),
-                "Tower Control": ProfileCreate.parse_reply('Ranks', row[Column.TC]),
-                "Rainmaker": ProfileCreate.parse_reply('Ranks', row[Column.RM]),
-                "Clam Blitz": ProfileCreate.parse_reply('Ranks', row[Column.CB]),
-            },
-            "stylepoints": eval(row[Column.SP]),
-            "cxp": int(row[Column.CXP]),
-        })
-        metaprofiles.append({
-            "_id": _id,
-            "signal": int(row[Column.SS]),
-            "smashgg": None,
-            "banned": None,
-            "reg": {
-                "reg": False,
-                "code": None,
-            }
-        })
-    return profiles, metaprofiles
+    @classmethod
+    def skip(cls, i, row):
+        """Check if a row should be skipped."""
+        if i < 2:
+            return True
+        try:
+            _id = int(row[cls.Column.ID][2:-1])
+        except NameError:
+            return True
+        else:
+            return _id
 
-
-def skip(i, row):
-    """Check if a row should be skipped."""
-    if i < 2:
-        return True
-    try:
-        _id = int(row[Column.ID][2:-1])
-    except NameError:
-        return True
-    else:
-        return _id
-
-
-class Column(IntEnum):
-    """Column names for the csv."""
-    ID = 14
-    IGN = 1
-    SW = 2
-    SZ = 3
-    TC = 4
-    RM = 5
-    CB = 6
-    SP = 8
-    CXP = 9
-    SS = 10
+    @classmethod
+    class Column(IntEnum):
+        """Column names for the csv."""
+        ID = 14
+        IGN = 1
+        SW = 2
+        SZ = 3
+        TC = 4
+        RM = 5
+        CB = 6
+        SP = 8
+        CXP = 9
+        SS = 10
