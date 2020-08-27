d7df01b395beaf97627054ce65cb8de5fd22e981
LeptoFlare <leptoflare@gmail.com>
2020-08-27 03:02:42 -0400

    Clean up the help command a bit fix some bugs, clean some things, QoL

--- offthedialbot/help.py
+++ offthedialbot/help.py
@@ -23,12 +23,12 @@ class HelpCommand(commands.DefaultHelpCommand):
             fields=[{
                 "name": "Commands:",
                 "value": "\n".join([
-                    f'`{self.clean_prefix}{command}` {command.short_doc}'
+                    self.short(command)
                     for command in await self.filter_commands(mapping[None]) if command.help])
             }, {
                 "name": "Misc Commands:",
                 "value": "\n".join([
-                    f'`{self.clean_prefix}{command}` {command.short_doc}'
+                    self.short(command)
                     for command in list_commands])
             }]
         )
@@ -37,12 +37,12 @@ class HelpCommand(commands.DefaultHelpCommand):
     async def send_cog_help(self, cog):
         """Send cog command page."""
         embed = self.create_embed(
-            title=f"{cog.qualified_name.capitalize()}",
+            title=cog.qualified_name.capitalize(),
             description=cog.description,
             fields=[{
                 "name": f"{cog.qualified_name.capitalize()} Commands:",
                 "value": "\n".join([
-                    f'`{self.clean_prefix}{command}` {command.short_doc}'
+                    self.short(command)
                     for command in cog.get_commands()
                 ])
             }]
@@ -52,13 +52,13 @@ class HelpCommand(commands.DefaultHelpCommand):
     async def send_group_help(self, group):
         """Send command group page."""
         embed = self.create_embed(
-            title=f"`{self.clean_prefix}{group}`",
+            title=self.short(group, False),
             description=group.help,
             fields=[{
                 "name": f"Subcommands:",
                 "value": "\n".join([
-                    f'`{self.clean_prefix}{command}` {command.short_doc}'
-                    for command in await self.filter_commands(group.all_commands.values())
+                    self.short(command)
+                    for command in await self.filter_commands(group.commands)
                 ])
             }]
         )
@@ -67,22 +67,21 @@ class HelpCommand(commands.DefaultHelpCommand):
     async def send_command_help(self, command):
         """Send command page."""
         embed = self.create_embed(
-            title=f"`{self.clean_prefix}{command}`",
+            title=self.short(command, False),
             description=command.help,
         )
         await self.get_destination().send(embed=embed)
 
     async def command_not_found(self, string):
         """Returns message when command is not found."""
-        return f"Command `{self.clean_prefix}{string}` does not exist."
+        return f"Command {self.short(string, False)} does not exist."
 
     async def subcommand_not_found(self, command, string):
         """Returns message when subcommand is not found."""
-        message = super().subcommand_not_found(command, string).split()
-        message[1] = f'`${message[1][1:-1]}`'
-        if not message[-1].endswith("."):
-            message[-1] = f'`{message[-1]}`.'
-        return " ".join(message)
+        if isinstance(command, Group) and len(command.all_commands) > 0:
+            return f"Command {self.short(command, False)} has no subcommand named `{string}`."
+        else:
+            return f"Command {self.short(command, False)} has no subcommands."
 
     async def send_error_message(self, error):
         """Send error message, override to support sending embeds."""
@@ -98,6 +97,10 @@ class HelpCommand(commands.DefaultHelpCommand):
         embed.set_footer(
             text=f"Type {self.clean_prefix}help command for more info on a command. You can also type {self.clean_prefix}help category for more info on a category.")
         return embed
+
+    def short(self, command, doc=True):
+        """List the command as a one-liner."""
+        return f'`{self.clean_prefix}{command}` {(command.short_doc if doc else "")}'
 
 
 help_command = HelpCommand()
