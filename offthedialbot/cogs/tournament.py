6655e0da7b57dd31c879f84f070b889893984ae0
LeptoFlare <leptoflare@gmail.com>
2020-12-23 20:33:46 -0500

    Create a check-in command

--- offthedialbot/cogs/tournament.py
+++ offthedialbot/cogs/tournament.py
@@ -16,53 +16,43 @@ class Tournament(commands.Cog, command_attrs={'hidden': True}):
     @tasks.loop(hours=1)
     async def loop(self):
         """Task loop that assigns roles."""
+        self.sync_competing()
+
+    @loop.before_loop
+    async def before_loop(self):
+        await self.bot.wait_until_ready()
+
+    async def sync_competing(self):
         docs = utils.firestore.db.collection(u'tournaments').document(u'2KnLtpnPNjz2AE0OxwX5').collection(u'signups').stream()
         ids = [doc.id for doc in docs]
 
         guild = self.bot.get_guild(374715620052172800)
         role = guild.get_role(415767083691802624)
 
-        for id in ids:
-            user = guild.get_member(int(id))
-            await user.add_roles(role)
-
         for sign in role.members:
             if not sign.id in ids:
                 await sign.remove_roles(role)
 
-    @loop.before_loop
-    async def before_loop(self):
-        await self.bot.wait_until_ready()
+        for id in ids:
+            user = guild.get_member(int(id))
+            await user.add_roles(role)
 
     @commands.command()
     @utils.deco.otd_only
-    @utils.deco.tourney()
-    @utils.deco.profile_required(competing=True)
     async def checkin(self, ctx: commands.Context):
         """Check in for the tournament."""
-        # Check if check-in is not enabled
-        if utils.tourney.get()['checkin'] is False:
-            raise commands.errors.DisabledCommand
-        # Check if the attendee has already checked in
-        if utils.roles.has(ctx.author, "Checked In"):
-            await utils.Alert(ctx, utils.Alert.Style.DANGER,
-                              title="Already Checked-in",
-                              description="You have already checked-in!")
-            raise utils.exc.CommandCancel
+        # Return if the user isn't competing
+        if not (role := utils.roles.get(ctx, "Competing")):
+            await ctx.message.add_reaction('❌')
+            return
 
         # Get Checked In role, or create it.
-        ui: utils.CommandUI = await utils.CommandUI(ctx,
-            discord.Embed(title="Check-in Form", color=utils.colors.COMPETING)
-        )
-        await Signup.profile_updated(ui, utils.Profile(ctx.author.id))
         if not (role := utils.roles.get(ctx, "Checked In")):
-            role = await ui.ctx.guild.create_role(name="Checked In")
+            role = await ctx.guild.create_role(name="Checked In")
 
         # Check the attendee in
-        await ui.ctx.author.add_roles(role)  # Add roles
-        utils.time.Timer.delete(destination=ctx.author.id, author=ctx.me.id)  # Remove warning
-
-        await ui.end(True)
+        await ctx.author.add_roles(role)  # Add roles
+        await ctx.message.add_reaction('✅')
 
     @commands.command(aliases=["competing", "registered"])
     @utils.deco.tourney()
