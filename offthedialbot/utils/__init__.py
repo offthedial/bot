5f596e37a563fcb4982da3b5a772b89f8bf13bcc
LeptoFlare <leptoflare@gmail.com>
2021-01-23 05:17:06 +0000

    Update utils/__init__ after removing a lot of the files

--- offthedialbot/utils/__init__.py
+++ offthedialbot/utils/__init__.py
@@ -1,15 +1,10 @@
 """Import common utilites used by all commands of the bot."""
 
-from .db import dbh
 from .session import session
 from . import channels
 from . import checks
 from . import colors
 from . import emojis
-from . import roles
-from . import firestore
-from . import time
-from . import tourney
 from . import smashgg
 from . import exceptions as exc
 from . import decorators as deco
@@ -17,4 +12,4 @@ from .maplist import Maplist
 from .command import Command
 from .alert import Alert
 from .command_ui import CommandUI
-from .profile import Profile, ProfileMeta
+from .db import Tournament, User
