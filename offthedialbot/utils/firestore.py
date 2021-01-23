9edfc32366540cb9fe81a4b04297ae069ff678f7
LeptoFlare <leptoflare@gmail.com>
2021-01-23 05:13:30 +0000

    Thanos snap the bot

--- offthedialbot/utils/firestore.py
+++
@@ -1,7 +0,0 @@
-from firebase_admin import credentials, firestore
-import firebase_admin
-
-cred = credentials.Certificate("service_account.json")
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
