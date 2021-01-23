d63903a923e91494196f045d4f22050c83eca101
LeptoFlare <leptoflare@gmail.com>
2021-01-23 05:26:09 +0000

    Update example slug for $to new

--- offthedialbot/commands/to/new.py
+++ offthedialbot/commands/to/new.py
@@ -32,7 +32,7 @@ class ToNew(utils.Command):
     @classmethod
     async def get_tourney_slug(cls, ui):
         """Create a new tournament."""
-        directions = f"Enter the full tournament slug (`it-s-dangerous-to-go-alone-november-2020`)"
+        directions = f"Enter the full tournament slug (`it-s-dangerous-to-go-alone-month-year`)"
         ui.embed.description = directions
         reply = await ui.get_reply()
         return reply.content
