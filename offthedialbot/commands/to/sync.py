522e9b218f175e2be06af67e62eba6f476660f9e
LeptoFlare <leptoflare@gmail.com>
2021-01-26 15:35:24 +0000

    Improve $to sync

--- offthedialbot/commands/to/sync.py
+++ offthedialbot/commands/to/sync.py
@@ -11,13 +11,16 @@ class ToSync(utils.Command):
     @classmethod
     @utils.deco.require_role("Staff")
     async def main(cls, ctx):
+        """Sync tournament."""
         await cls.sync(ctx.bot)
-    
+        await ctx.message.add_reaction('♻️')
+
     @staticmethod
-    async def sync(bot):
-        """ Synchronize smash.gg tournament data and competing roles."""
+    async def sync(bot, smashgg=True):
+        """Synchronize smash.gg tournament data and competing roles."""
         tourney = utils.Tournament()
-        await tourney.sync_smashgg()
+        if smashgg:
+            await tourney.sync_smashgg()
 
         # Sync competing roles
         if not (docs := tourney.signups()):
