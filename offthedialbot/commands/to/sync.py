60bf160fff5fcb45e479414a83676db032bd0d7f
LeptoFlare <leptoflare@gmail.com>
2021-02-12 07:09:26 +0000

    Update $to sync to sync signal strength milestone roles

--- offthedialbot/commands/to/sync.py
+++ offthedialbot/commands/to/sync.py
@@ -14,13 +14,22 @@ class ToSync(utils.Command):
         await cls.sync(ctx.bot)
         await ctx.message.add_reaction('♻️')
 
+    @classmethod
+    async def sync(cls, bot, smashgg=True):
+        tourney = await cls.sync_tournament(smashgg)
+        guild = await cls.sync_competing(bot, tourney)
+        await cls.sync_signal_strength(guild)
+
     @staticmethod
-    async def sync(bot, smashgg=True):
+    async def sync_tournament(smashgg):
         tourney = utils.Tournament()
         if smashgg:
             await tourney.sync_smashgg()
+        return tourney
 
-        # Sync competing roles
+    @staticmethod
+    async def sync_competing(bot, tourney):
+        """Sync competing roles."""
         if docs := tourney.signups():
             ids = [int(doc.id) for doc in docs]
         else:
@@ -35,3 +44,22 @@ class ToSync(utils.Command):
         for id in ids:
             user = guild.get_member(id)
             await user.add_roles(role)
+
+        return guild
+
+    @staticmethod
+    async def sync_signal_strength(guild):
+        """Add signal strength roles to users."""
+        users = utils.db.collection(u'users').where(u'meta.signal', u'>', 999).stream()
+        role_1k = guild.get_role(809674067380666400)
+        role_5k = guild.get_role(809674415867428936)
+        # Loop over users
+        for user in users:
+            user = utils.User(user.id)
+            discord = await user.discord(guild)
+            signal = user.dict["meta"]["signal"]
+            # Add roles depending on milestones
+            if signal >= 1000:
+                await discord.add_roles(role_1k)
+            if signal >= 5000:
+                await discord.add_roles(role_5k)
