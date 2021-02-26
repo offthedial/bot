01f5eaf9e75800e71d7e95a90c750f9f604f45dd
LeptoFlare <leptoflare@lepto.tech>
2021-02-26 14:41:24 +0000

    Update Signup to store user within class instance

--- offthedialbot/utils/db/signup.py
+++ offthedialbot/utils/db/signup.py
@@ -5,8 +5,9 @@ from . import Tournament
 class Signup:
     """Represents what useSignup would be."""
 
-    def __init__(self, id=None, tourney=None):
+    def __init__(self, id, user, tourney=None):
         self.id = str(id)
+        self.user = user
         self.tourney = tourney if tourney else Tournament()
 
         if not isinstance(self.id, (int, str)):
@@ -31,7 +32,7 @@ class Signup:
         self.col = self.ref.parent.id
         self.dict = self.doc.to_dict()
 
-    async def smashgg(self, user):
+    async def smashgg(self):
         """Get smash.gg data from the api with the user slug."""
         query = """query($slug: String) {
           tournament(slug: $slug) {
@@ -47,5 +48,5 @@ class Signup:
         }"""
         status, data = await utils.graphql("smashgg", query, {"slug": self.tourney.dict["slug"]})
         for participant in data["data"]["tournament"]["participants"]["nodes"]:
-            if participant["user"]["slug"][5:] == user.dict["profile"]["smashgg"]:
+            if participant["user"]["slug"][5:] == self.user.dict["profile"]["smashgg"]:
                 return participant["gamerTag"]
