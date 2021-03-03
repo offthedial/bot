0702034536603bf875e1dd49bd7f8b9f1546b74c
LeptoFlare <leptoflare@lepto.tech>
2021-03-03 07:14:44 +0000

    Remove welcome message when a member joins

--- offthedialbot/listeners/on_member_join.py
+++ offthedialbot/listeners/on_member_join.py
@@ -11,31 +11,9 @@ async def on_member_join(client, member):
         return
 
     await add_roles(client, member)  # Add roles
-    welcome = await send_welcome(client, member)  # Create welcome message
-
-    # Incase they leave immediately
-    await member_leave(client, member, welcome)
 
 
 async def add_roles(client, member):
     """Add roles to the user."""
     role = member.guild.get_role(427710343616397322)
     await member.add_roles(role)
-
-
-async def send_welcome(client, member):
-    """Send welcome message."""
-    channel = utils.channels.general(client)
-    welcome = await channel.send(f"Let's welcome {member.mention} to __Off the Dial__! :wave:")
-    await welcome.add_reaction("\U0001f44b")
-    return welcome
-
-
-async def member_leave(client, member, welcome):
-    """Delete the welcome message because the user left less than 5 minutes after."""
-    try:
-        await client.wait_for('member_remove', check=utils.checks.member(member), timeout=300)
-    except asyncio.TimeoutError:
-        pass
-    else:
-        await welcome.delete()
