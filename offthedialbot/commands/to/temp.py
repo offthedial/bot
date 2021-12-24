7464f4822e270fed627a3df88386304c90d53e88
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2021-12-24 02:22:48 +0000

    Fix errors with temp.py

--- offthedialbot/commands/to/temp.py
+++ offthedialbot/commands/to/temp.py
@@ -3,29 +3,35 @@ from offthedialbot import utils
 
 
 class ToTemp(utils.Command):
-
     @classmethod
     @utils.deco.require_role("Staff")
     async def main(cls, ctx, confirm=False):
         """Replace each document in the collection."""
-        docs = utils.db.collection(u'users').stream()
+        docs = utils.db.collection("users").stream()
         batch = utils.db.batch()
         for doc in docs:
             profile_data = doc.to_dict()["profile"]
 
             # find rank with the highest power and return rank
-            all_powers = [utils.User.rank_to_power(rank) for rank in profile_data["ranks"].values()]
-            highest_power = max(all_powers)
-            rank = [rank for rank in profile_data["ranks"].values() if utils.User.rank_to_power(rank) == highest_power][0]
+            rank = (
+                max(
+                    profile_data["ranks"].values(),
+                    key=lambda x: utils.User.rank_to_power(x),
+                )
+                if profile_data.get("ranks")
+                else None
+            )
 
             # create new profile object
             profile = {
-                "ign": profile_data["ign"],
-                "sw": profile_data["sw"],
+                "ign": profile_data.get("ign"),
+                "sw": profile_data.get("sw"),
                 "rank": rank,
                 "weapons": None,
                 "cxp": None,
-                "smashgg": f"smash.gg/user/{profile_data['smashgg']}"
+                "smashgg": None
+                if not profile_data.get("smashgg")
+                else f"smash.gg/user/{profile_data['smashgg']}",
             }
             batch.update(doc.reference, {"profile": profile})
 
