a0a7f710644c6309af839775e323fb2e4e388437
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2022-01-09 19:44:27 -0500

    Update temp.py

--- offthedialbot/commands/to/temp.py
+++ offthedialbot/commands/to/temp.py
@@ -2,13 +2,59 @@
 from offthedialbot import utils
 
 
+class FirestoreAutoWriteBatch:
+    def __init__(self, batch, limit=100, auto_commit=True):
+        self._batch = batch
+        self._limit = limit
+        self._auto_commit = auto_commit
+        self._count = 0
+
+    def create(self, *args, **kwargs):
+        self._batch.create(*args, **kwargs)
+        self._count += 1
+
+        if self._auto_commit:
+            self.commit_if_limit()
+
+    def set(self, *args, **kwargs):
+        self._batch.set(*args, **kwargs)
+        self._count += 1
+
+        if self._auto_commit:
+            self.commit_if_limit()
+
+    def update(self, *args, **kwargs):
+        self._batch.update(*args, **kwargs)
+        self._count += 1
+
+        if self._auto_commit:
+            self.commit_if_limit()
+
+    def delete(self, *args, **kwargs):
+        self._batch.delete(*args, **kwargs)
+        self._count += 1
+
+        if self._auto_commit:
+            self.commit_if_limit()
+
+    def commit(self, *args, **kwargs):
+        self._batch.commit()
+        self._count = 0
+
+    def commit_if_limit(self):
+        if self._count > self._limit:
+            self._batch.commit()
+            self._count = 0
+
+            
 class ToTemp(utils.Command):
     @classmethod
     @utils.deco.require_role("Staff")
     async def main(cls, ctx, commit=False):
         """Replace each document in the collection."""
         docs = utils.db.collection("users").stream()
-        batch = utils.db.batch()
+        batch = FirestoreAutoWriteBatch(utils.db.batch())
+
         for doc in docs:
             profile_data = doc.to_dict()["profile"]
 
