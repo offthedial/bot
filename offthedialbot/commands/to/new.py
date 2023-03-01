5ec05b4175c3cdcb9d33fac07fe2e109d596967e
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2023-03-01 09:33:25 -0500

    Update how whitelist is determined

--- offthedialbot/commands/to/new.py
+++ offthedialbot/commands/to/new.py
@@ -45,7 +45,14 @@ class ToNew(utils.Command):
     @classmethod
     async def get_tourney_type(cls, ui):
         """Create a new tournament."""
-        directions = f"Enter the tournament type (`idtga`, `wl`)"
+        directions = "\n".join([
+            f"Enter the tournament type (`idtga`, `wl`, `https://fabl.otd.ink`).",
+            "This will be used in the link on the website.",
+            "If you want to link to a different site other than otd.ink, enter the full url, for example:",
+            " - `idtga` -> `otd.ink/idtga`",
+            " - `wl` -> `otd.ink/wl`",
+            " - `https://fabl.otd.ink` -> `fabl.otd.ink`"
+        ])
         ui.embed.description = directions
         reply = await ui.get_reply()
         return reply.content.lower()
